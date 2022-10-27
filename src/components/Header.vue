3a1a432c0c671a30c26c7d213521876c54967bd7
KatsuroDev <katsuro.userg@gmail.com>
2022-10-27 02:29:06 -0400

    add Header component start

---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<script setup>
+</script>
+
+<template>
+    <div class="row d-flex align-items-stretch">
+        <div class="col title">
+            <h1>Kat's Portfolio</h1>
+        </div>
+        <div class="col-auto">
+            <ul>
+                <li>Albums</li>
+                <li>Files</li>
+                <li>About</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.title {
+    display: flex;
+    align-items: center;
+}
+
+h1 {
+    display: block;
+}
+
+div {
+    color: var(--platinum);
+}
+
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    height:100%;
+}
+
+li {
+    padding: 5px;
+    display: inline;
+}
+
+.row {
+    height: 100px;
+    background-color: var(--jet);
+}
+
+</style>
