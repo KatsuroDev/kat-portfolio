954f6d3732c0f4de92a7717f4e879e4689f3a99b
KatsuroDev <katsuro.userg@gmail.com>
2022-10-31 15:19:13 -0400

    add carousel component

--- src/views/pages/HomePage.vue
+++ src/views/pages/HomePage.vue
@@ -1,11 +1,13 @@
 <script setup>
     import Palette from '../../components/Palette.vue'
+    import Carousel from '../../components/Carousel.vue';
     import MainLayout from '../layouts/MainLayout.vue'
 </script>
 
 <template>
  <MainLayout>
     <Palette />
+    <Carousel />
  </MainLayout>
 </template>
 
