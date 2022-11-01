56584e06f7631c2d03487acc8b7ac61748933940
KatsuroDev <katsuro.userg@gmail.com>
2022-10-31 20:37:34 -0400

    add basic carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,7 +1,36 @@
 <script setup>
+
+import { onMounted } from 'vue';
+import * as bootstrap from 'bootstrap';
+
+onMounted(() => {
+    const myCarousel = new bootstrap.Carousel('#carouselExampleInterval')
+})
+
 </script>
 
 <template>
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="2000">
+      <img src="../assets/images/picture1.JPG" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="../assets/images/picture2.JPG" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="../assets/images/picture3.JPG" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 </template>
 
 <style scoped>
