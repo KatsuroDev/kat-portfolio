86f17794c50873a244ef0cec549b90a2f1fa31b5
KatsuroDev <katsuro.userg@gmail.com>
2022-11-29 23:22:55 -0500

    add custom file selector

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -1,10 +1,43 @@
 <script setup>
+
+    const props = defineProps({
+    value: String
+    })
+    const emit = defineEmits(['input'])
+
+    function handleFileChange(e) {
+      // Whenever the file changes, emit the 'input' event with the file data.
+      emit('input', e.target.files)
+    }
 </script>
 
+
 <template>
-    <div>UPLOAD</div>
-    <input type="file" multiple="true" required="true" accept="image/*"/>
+    <!-- <input type="file" multiple="true" required="true" accept="image/*"/> -->
+    <label class="file-select">
+        <div class="btn select-button">
+            <span v-if="value">Selected File: {{ value.name }}</span>
+            <span v-else> Select File </span>
+        </div>
+        <input @change="handleFileChange" type="file" multiple="true" required="true" accept="image/*" />
+    </label>
 </template>
 
 <style scoped>
+.file-select > .select-button {
+  padding: 1rem;
+
+  color: var(--platinum);
+  background-color: var(--iris);
+
+  border-radius: .3rem;
+
+  text-align: center;
+  font-weight: bold;
+}
+
+/* Don't forget to hide the original file input! */
+.file-select > input[type="file"] {
+  display: none;
+}
 </style>
--- src/views/pages/UploadPage.vue
+++ src/views/pages/UploadPage.vue
@@ -1,11 +1,19 @@
 <script setup>
+    import { ref, watch } from 'vue'
+
     import MainLayout from '../layouts/MainLayout.vue'
     import Uploader from '../../components/Uploader.vue'
+
+    const files = ref([])
+
 </script>
 
 <template>
  <MainLayout>
-    <Uploader/>
+    <p>My File Selector: <Uploader v-model="files" @input="(newFiles) => files = newFiles"></Uploader></p>
+    <p v-for="file in files" :key="file.id">
+        {{file.name}}
+    </p>
  </MainLayout>
 </template>
 
